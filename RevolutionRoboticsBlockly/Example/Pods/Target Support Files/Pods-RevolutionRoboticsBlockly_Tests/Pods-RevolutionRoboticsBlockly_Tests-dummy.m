4d0d7c19ea374275d8be068266bf457a4495169f
Mate Papp <mate.papp@supercharge.io>
2019-04-03 14:47:07 +0200

    Update gitignore

--- RevolutionRoboticsBlockly/Example/Pods/Target Support Files/Pods-RevolutionRoboticsBlockly_Tests/Pods-RevolutionRoboticsBlockly_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RevolutionRoboticsBlockly_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_RevolutionRoboticsBlockly_Tests
-@end
--- RevolutionRoboticsBlockly/Example/Pods/Target Support Files/RevolutionRoboticsBlockly/RevolutionRoboticsBlockly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RevolutionRoboticsBlockly : NSObject
-@end
-@implementation PodsDummy_RevolutionRoboticsBlockly
-@end
